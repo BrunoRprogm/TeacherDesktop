1f630ee94fee414d234c0a08a95061abc3971d3e
BrunoRprogm <brunoramonnn986@gmail.com>
2025-05-29 21:16:26 -0400

    Conectado com o novo banco de dados, agora solo.

--- src/main/java/com/senai/teacherapp/DAO/ConnectDB.java
+++ src/main/java/com/senai/teacherapp/DAO/ConnectDB.java
@@ -6,6 +6,13 @@ import java.sql.SQLException;
 
 public class ConnectDB {
     public static Connection connectDB() throws SQLException{
-        return DriverManager.getConnection("jdbc:mysql://localhost:3306/teacherapp","root","");
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/saep_db","root","");
     }
-}
+
+    public static void main(String[] args) {
+        try(Connection conn = ConnectDB.connectDB()){
+            System.out.println("Connected to database");
+        }catch (SQLException e){
+            System.out.println("Error connecting to database"+e);}
+    }
+    }
