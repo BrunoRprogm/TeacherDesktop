7ec71d61133124c11f9076f90873d371a3dc1982
BrunoRprogm <brunoramonnn986@gmail.com>
2025-06-08 22:22:24 -0400

    add java.desktop

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,10 +2,12 @@ module com.senai.teacherdesktop {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires java.desktop;
 
 
     opens com.senai.teacherdesktop to javafx.fxml;
     exports com.senai.teacherdesktop;
     exports com.senai.teacherdesktop.Controllers;
     opens com.senai.teacherdesktop.Controllers to javafx.fxml;
+    opens com.senai.teacherdesktop.Models to javafx.base;
 }
