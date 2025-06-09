66fc6985fc10d0dc0b288f7c21f80d64ec5cf9f3
BrunoRprogm <brunoramonnn986@gmail.com>
2025-06-08 22:31:24 -0400

    métodos do banco de dados, como listar turma, cadastrar uma nova turma, e excluir uma turma.

--- src/main/java/com/senai/teacherdesktop/DAO/SchoolClassDAO.java
+++ src/main/java/com/senai/teacherdesktop/DAO/SchoolClassDAO.java
@@ -1,4 +1,58 @@
 package com.senai.teacherdesktop.DAO;
 
+import com.senai.teacherdesktop.Models.SchoolClass;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class SchoolClassDAO {
+
+    public static List<SchoolClass> listClass() throws SQLException {
+        List<SchoolClass> schoolClasses = new ArrayList<>();
+        String sql = "SELECT * FROM turma";
+
+        try( Connection conn = ConnectDB.connectDB();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery("SELECT * FROM turma");){
+            while(rs.next()){
+                SchoolClass turma1 = new SchoolClass(rs.getInt("idTurma"),rs.getString("nm_turma"));
+                schoolClasses.add(turma1);
+            }
+        }
+        return schoolClasses;
+    }
+
+    public void cadastrarTurma(SchoolClass sc) throws SQLException{
+
+        String sql = "INSERT INTO turma (nm_turma) VALUES (?)";
+
+        try(Connection conn = ConnectDB.connectDB();
+            PreparedStatement prst = conn.prepareStatement(sql)){
+            prst.setString(1,sc.getNm_turma());
+            prst.executeUpdate();
+        }
+
+    }
+
+    public void excluirTurma(int idTurma) throws SQLException{
+
+        String sql = "DELETE FROM turma where idTurma = ?";
+
+        try( Connection conn = ConnectDB.connectDB();
+        PreparedStatement prst = conn.prepareStatement(sql)){
+            prst.setInt(1,idTurma);
+            prst.executeUpdate();
+        }
+
+    }
+
+    public void excluirTurmaPorId(int id) throws SQLException{
+        String sql = "DELETE from turma where idTurma ?";
+        try(Connection conn = ConnectDB.connectDB();
+        PreparedStatement stmt = conn.prepareStatement(sql)){
+            stmt.setInt(1,id);
+            stmt.executeUpdate();
+        }
+    }
+
 }
