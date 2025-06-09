ec42b5eaebfe57b49a9d4b5348a8eecd768fa292
BrunoRprogm <brunoramonnn986@gmail.com>
2025-06-08 22:32:02 -0400

    Instanciação de Usuário

--- src/main/java/com/senai/teacherdesktop/Models/User.java
+++ src/main/java/com/senai/teacherdesktop/Models/User.java
@@ -1,4 +1,42 @@
 package com.senai.teacherdesktop.Models;
 
-public class User {
+public class User  {
+
+   private String email;
+   private String senha;
+   private Integer id;
+   private String nome;
+
+
+    public User(int id, String email, String senha, String string) {
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public User(String email, String senha, Integer id, String nome) {
+        this.email = email;
+        this.senha = senha;
+        this.id = id;
+        this.nome = nome;
+    }
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getInt(){
+        return id;
+    }
+
+
 }
