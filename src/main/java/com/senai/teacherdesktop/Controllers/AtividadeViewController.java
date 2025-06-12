057a3f9c65164394dffb71ae65cd79a52f775f06
BrunoRprogm <brunoramonnn986@gmail.com>
2025-06-11 22:52:28 -0400

    ....

---
+++ src/main/java/com/senai/teacherdesktop/Controllers/AtividadeViewController.java
@@ -0,0 +1,19 @@
+package com.senai.teacherdesktop.Controllers;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+
+public class AtividadeViewController {
+
+    @FXML
+    private TableColumn<Integer, Integer> colunaId;
+
+
+
+
+}
+
+
+
+
